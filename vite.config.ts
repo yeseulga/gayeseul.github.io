23d252a4e5250dcda449d826014bce39f3b36a81
Yeseul Ga <gayeseul7@gmail.com>
2025-12-22 21:47:04 +0900

    remove Lovable, initial deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // 브라우저용 crypto polyfill
 import { Buffer } from "buffer";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   base: "/gayeseul.github.io/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()], // componentTagger 제거
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      crypto: "crypto-browserify", // crypto polyfill
-      buffer: "buffer", // Buffer polyfill
+      crypto: "crypto-browserify",
+      buffer: "buffer",
     },
   },
   define: {
-    global: {}, // 일부 라이브러리용 글로벌 객체
+    global: {},
   },
-}));
+});
